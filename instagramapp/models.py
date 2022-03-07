cb0f11cd54ca8e3c3f7b2efea69406126c3bf84e
email <kibetdavidro@gmail.com>
2022-03-07 15:24:12 +0300

    define profile class model

--- instagramapp/models.py
+++ instagramapp/models.py
@@ -50,3 +50,14 @@ class Image(models.Model):
 
     def __str__(self):
         return self.image_name
+
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(max_length=1200, null=True, blank=True)
+    profile_photo = CloudinaryField('image')
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_on']
