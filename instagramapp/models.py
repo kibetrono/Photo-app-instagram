4a8c576bbb9d0c6a407dd56701b00c57b3233fb5
email <kibetdavidro@gmail.com>
2022-03-09 15:27:30 +0300

    deployment to heroku

--- instagramapp/models.py
+++ instagramapp/models.py
@@ -6,11 +6,11 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Image(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='images')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='images',null=True,blank="True")
     image_name = models.CharField(max_length=100)
     image_caption = models.TextField(max_length=1000)
     image = CloudinaryField('image')
-    profile = models.ForeignKey(User, on_delete=models.CASCADE)
+    profile = models.ForeignKey(User, on_delete=models.CASCADE,null=True,blank="True")
     likes_number = models.IntegerField(default=0)
     comments_number = models.IntegerField(default=0)
     created_on = models.DateTimeField(auto_now_add=True)
